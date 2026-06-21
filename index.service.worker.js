3f2da9bc7902bb0b652acf7968a007823432e161
Dams4K <dams4k@users.noreply.github.com>
2026-06-21 15:02:22 +0200

    new version

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779817012|7363209486';
+const CACHE_VERSION = '1782046930|11423960493';
 /** @type {string} */
 const CACHE_PREFIX = 'Elementium-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
